9d23584df9cb37a17b6aa090728b13b9b27267ab
Vic Chang <vicchang@12cm.com.tw>
2023-10-09 15:35:21 +0800

    discovery server nexflix eureka

--- order-service/src/main/java/com/vic/sp/orderservice/service/OrderService.java
+++ order-service/src/main/java/com/vic/sp/orderservice/service/OrderService.java
@@ -23,7 +23,7 @@ import java.util.UUID;
 public class OrderService {
 
     private final OrderRepository orderRepository;
-    private final WebClient webClient;
+    private final WebClient.Builder webClientBuilder;
 
     public void placeOrder(OrderRequest orderRequest) {
         Order order = new Order();
@@ -43,8 +43,8 @@ public class OrderService {
 
         // call Inventory Service, and place order if product is in
         // stock
-        InventoryResponse[] inventoryResponseArray = webClient.get()
-                .uri("http://localhost:8082/api/inventory",
+        InventoryResponse[] inventoryResponseArray = webClientBuilder.build().get()
+                .uri("http://inventory-service/api/inventory",
                         uriBuilder -> uriBuilder.queryParam("skuCode", skuCodes).build())
                 .retrieve()
                 .bodyToMono(InventoryResponse[].class)
--- product-service/src/main/java/com/vic/sp/productservice/ProductServiceApplication.java
+++ product-service/src/main/java/com/vic/sp/productservice/ProductServiceApplication.java
@@ -2,13 +2,12 @@ package com.vic.sp.productservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.domain.EntityScan;
-import org.springframework.context.annotation.ComponentScan;
-import org.springframework.context.annotation.ComponentScans;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 
 
 @SpringBootApplication
+@EnableDiscoveryClient
 
 public class ProductServiceApplication {
 
